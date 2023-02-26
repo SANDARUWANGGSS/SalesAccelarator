2586786e7784b7ce447838977504cebd38a6c0e9
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-02-26 13:41:58 +0530

    Spring security enabled

---
+++ src/main/java/com/example/SRM/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.SRM.controllers;
+
+import com.example.SRM.dao.UserDao;
+import com.example.SRM.dto.AuthenticationRequest;
+import com.example.SRM.security.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class AuthController
+{
+    private final AuthenticationManager authenticationManager;
+    private final UserDao userDao;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(
+            @RequestBody AuthenticationRequest request
+            ){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
+        );
+
+        final UserDetails user = userDao.findUserByEmail(request.getEmail());
+        if(user != null)
+        {
+            return ResponseEntity.ok(jwtUtils.generateToken(user));
+        }
+        return ResponseEntity.status(400).body("Some error has occured");
+    }
+}
